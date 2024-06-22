c6d84dee4445a93123594f043d18d85dd4bd2cd4
whitecode <guvalakat1603@gmail.com>
2024-06-22 16:49:42 +0500

    changed

--- src/component/MobileMenu.tsx
+++ src/component/MobileMenu.tsx
@@ -1,6 +1,12 @@
 const MobileMenu = () => {
     return (
-        <div>MobileMenu</div>
+        <div>
+            <div className='flex flex-col cursor-pointer'>
+                <div className={'w-6 h-1 bg-blue-500 rounded-sm'} ></div>
+                <div className={'w-6 h-1 bg-blue-500 rounded-sm'} ></div>
+                <div className={'w-6 h-1 bg-blue-500 rounded-sm'} ></div>
+            </div>
+        </div>
     )
 }
 
