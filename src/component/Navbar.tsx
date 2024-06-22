dc4fa2be016f9d8bea9e5bce7e806086f2edb2c1
whitecode <guvalakat1603@gmail.com>
2024-06-22 16:40:14 +0500

    codes are .added

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,6 +1,20 @@
+import Link from "next/link";
+import MobileMenu from "@/component/MobileMenu";
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={'h-24 flex items-center justify-between'}>
+            {/*LEFT*/}
+            <div className={''}>
+                <Link href={"/"} className={'font-bold text-xl text-blue-500'}>T-SOCIAL</Link>
+            </div>
+            {/*CENTER*/}
+            <div className={'hidden'}></div>
+            {/*RIGHT*/}
+            <div className={'w-full h-4'}>
+                <MobileMenu />
+            </div>
+        </div>
     )
 }
 export default Navbar;
