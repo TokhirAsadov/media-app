02079e127ccbca55a1bed1e2ab9f445260545e52
Takhir <guvalakat1603@gmail.com>
2024-06-24 14:19:35 +0500

    style is added

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const AddPost = () =>{
     return (
-        <div className={'p-4 bg-white rounded-lg flex gap-4 justify-between text-sm'}>
+        <div className={'p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'}>
             {/*    AVATAR   */}
             <Image src={"https://images.pexels.com/photos/21937092/pexels-photo-21937092.jpeg"} alt={""} width={48} height={48} className="w-12 h-12 object-cover rounded-full" />
             {/*    POST    */}
