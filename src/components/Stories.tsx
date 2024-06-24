2fddc152eade8aaedee7032d9ac8a92b6e5dbb5d
Takhir <guvalakat1603@gmail.com>
2024-06-24 13:32:05 +0500

    Details of Stories.tsx are done

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,6 +1,51 @@
+import Image from "next/image";
+
 const Stories = () =>{
     return (
-        <div>Stories</div>
+        <div className={'p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide'}>
+            <div className="flex gap-8 w-max">
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+                {/* STORY */}
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src={"https://images.pexels.com/photos/19345473/pexels-photo-19345473.jpeg"} alt={""} width={80} height={80}  className={"w-20 h-20 rounded-full ring-2"}/>
+                    <span className={"font-medium"}>Takhir</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
