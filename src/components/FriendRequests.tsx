0e83ba675d3f2c11f6a71fabf684f4b3567d4c22
Takhir <guvalakat1603@gmail.com>
2024-06-25 11:07:34 +0500

    Details of FriendRequests.tsx are done

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,7 +1,99 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const FriendRequests = () => {
     return (
-        <div>
-            FriendRequests
+        <div className={'p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'}>
+            {/*    TOP  */}
+            <div className="flex justify-between items-center font-medium">
+                <span className={'text-gray-500'}>Friend Requests</span>
+                <Link href={"/"} className={'text-blue-500 text-xs'}>See all</Link>
+            </div>
+            {/*  User  */}
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Image
+                        src={"https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-coffee-pot-and-coffee-cup-on-window-sill.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
+                        alt={""}
+                        width={40}
+                        height={40}
+                        className={'w-10 h-10 rounded-full object-cover'}
+                    />
+                    <span className={'font-semibold'}>John Wick</span>
+                </div>
+                <div className="flex gap-3 justify-end">
+                    <Image
+                        src={"/accept.png"}
+                        alt={""}
+                        width={20}
+                        height={20}
+                        className={'cursor-pointer'}
+                    />
+                    <Image
+                        src={"/reject.png"}
+                        alt={""}
+                        width={20}
+                        height={20}
+                        className={'cursor-pointer'}
+                    />
+                </div>
+            </div>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Image
+                        src={"https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-coffee-pot-and-coffee-cup-on-window-sill.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
+                        alt={""}
+                        width={40}
+                        height={40}
+                        className={'w-10 h-10 rounded-full object-cover'}
+                    />
+                    <span className={'font-semibold'}>John Wick</span>
+                </div>
+                <div className="flex gap-3 justify-end">
+                    <Image
+                        src={"/accept.png"}
+                        alt={""}
+                        width={20}
+                        height={20}
+                        className={'cursor-pointer'}
+                    />
+                    <Image
+                        src={"/reject.png"}
+                        alt={""}
+                        width={20}
+                        height={20}
+                        className={'cursor-pointer'}
+                    />
+                </div>
+            </div>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Image
+                        src={"https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-coffee-pot-and-coffee-cup-on-window-sill.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
+                        alt={""}
+                        width={40}
+                        height={40}
+                        className={'w-10 h-10 rounded-full object-cover'}
+                    />
+                    <span className={'font-semibold'}>John Wick</span>
+                </div>
+                <div className="flex gap-3 justify-end">
+                    <Image
+                        src={"/accept.png"}
+                        alt={""}
+                        width={20}
+                        height={20}
+                        className={'cursor-pointer'}
+                    />
+                    <Image
+                        src={"/reject.png"}
+                        alt={""}
+                        width={20}
+                        height={20}
+                        className={'cursor-pointer'}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
