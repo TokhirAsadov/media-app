d283e818d59cc093dea4e93543e7ecf386b22b8f
Takhir <guvalakat1603@gmail.com>
2024-06-25 12:15:02 +0500

    Details of RightMenu.tsx are done

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,6 +1,24 @@
-const RightMenu = () =>{
+import FriendRequests from "@/components/FriendRequests";
+import Birthdays from "@/components/Birthdays";
+import Ad from "@/components/Ad";
+import UserInfoCard from "@/components/UserInfoCard";
+import UserMediaCard from "@/components/UserMediaCard";
+
+const RightMenu = ({userId}:{userId?:string}) =>{
     return (
-        <div>RightMenu</div>
+        <div className={"flex flex-col gap-6"}>
+            {
+                userId ? (
+                    <>
+                        <UserInfoCard userId={userId} />
+                        <UserMediaCard userId={userId} />
+                    </>
+                ):null
+            }
+            <FriendRequests />
+            <Birthdays />
+            <Ad size={"md"} />
+        </div>
     )
 }
 
