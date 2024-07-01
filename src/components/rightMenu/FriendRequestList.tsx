65a481255d2a3cfa9479464401006adfa908ae22
Takhir <guvalakat1603@gmail.com>
2024-07-01 10:47:43 +0500

    details of FriendRequestList.tsx are done

--- src/components/rightMenu/FriendRequestList.tsx
+++ src/components/rightMenu/FriendRequestList.tsx
@@ -1,1 +1,93 @@
 "use client"
+
+import Image from "next/image";
+import {FollowRequest, User} from "@prisma/client";
+import {useOptimistic, useState} from "react";
+import {state} from "sucrase/dist/types/parser/traverser/base";
+import {acceptFollowRequest, declineFollowRequest} from "@/lib/actions";
+
+type RequestWithUser = FollowRequest & {
+    sender: User
+}
+
+const FriendRequestList = ({requests}:{requests: RequestWithUser[]}) => {
+
+    const [requestState, setRequestState] = useState(requests)
+
+    const accept = async (requestId: number, userId: string) => {
+        removeOptimisticRequest(requestId);
+
+        try {
+            await acceptFollowRequest(userId);
+            setRequestState(prevState => prevState.filter(req=>req.id!==requestId))
+        }
+        catch (err){
+            console.log(err)
+        }
+    }
+
+    const decline = async (requestId: number, userId: string) => {
+        removeOptimisticRequest(requestId);
+
+        try {
+            await declineFollowRequest(userId);
+            setRequestState(prevState => prevState.filter(req=>req.id!==requestId))
+        }
+        catch (err){
+            console.log(err)
+        }
+    }
+
+    const [optimisticRequest,removeOptimisticRequest] = useOptimistic(
+        requestState,
+        (state, value: number)=>state.filter(req => req.id!==value)
+    );
+
+    return (
+        <div className={''}>
+            {
+                optimisticRequest.map((request:RequestWithUser) => (
+                    <div key={request.id} className="flex items-center justify-between">
+                        <div className="flex items-center gap-4">
+                            <Image
+                                src={request.sender.avatar || "/noAvatar.png"}
+                                alt={""}
+                                width={40}
+                                height={40}
+                                className={'w-10 h-10 rounded-full object-cover'}
+                            />
+                            <span className={'font-semibold'}>{(request.sender.name && request.sender.surname) ? request.sender.name+" "+request.sender.surname : request.sender.username}</span>
+                        </div>
+                        <div className="flex gap-3 justify-end">
+                            <form action={() => accept(request.id, request.sender.id)}>
+                                <button>
+                                    <Image
+                                        src={"/accept.png"}
+                                        alt={""}
+                                        width={20}
+                                        height={20}
+                                        className={'cursor-pointer'}
+                                    />
+                                </button>
+                            </form>
+                            <form action={() => decline(request.id, request.sender.id)}>
+                                <button>
+                                    <Image
+                                        src={"/reject.png"}
+                                        alt={""}
+                                        width={20}
+                                        height={20}
+                                        className={'cursor-pointer'}
+                                    />
+                                </button>
+                            </form>
+
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default FriendRequestList;
