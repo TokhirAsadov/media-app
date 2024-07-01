e08259bc80e9bb65c132c5da39345da57924af68
Takhir <guvalakat1603@gmail.com>
2024-07-01 10:47:51 +0500

    details of FriendRequest.tsx are done

--- src/components/rightMenu/FriendRequests.tsx
+++ src/components/rightMenu/FriendRequests.tsx
@@ -1,7 +1,25 @@
 import Link from "next/link";
-import Image from "next/image";
+import {auth} from "@clerk/nextjs/server";
+import prisma from "@/lib/client";
+import FriendRequestList from "@/components/rightMenu/FriendRequestList";
 
-const FriendRequests = () => {
+const FriendRequests = async () => {
+
+    const {userId} = auth();
+
+    if (!userId) return null;
+
+    const requests = await prisma.followRequest.findMany({
+        where: {
+            receiverId: userId,
+        },
+        include: {
+            sender: true,
+        },
+    });
+
+    if (requests.length ===0) return null;
+
     return (
         <div className={'p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'}>
             {/*    TOP  */}
@@ -10,90 +28,7 @@ const FriendRequests = () => {
                 <Link href={"/"} className={'text-blue-500 text-xs'}>See all</Link>
             </div>
             {/*  User  */}
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                    <Image
-                        src={"https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-coffee-pot-and-coffee-cup-on-window-sill.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
-                        alt={""}
-                        width={40}
-                        height={40}
-                        className={'w-10 h-10 rounded-full object-cover'}
-                    />
-                    <span className={'font-semibold'}>John Wick</span>
-                </div>
-                <div className="flex gap-3 justify-end">
-                    <Image
-                        src={"/accept.png"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                        className={'cursor-pointer'}
-                    />
-                    <Image
-                        src={"/reject.png"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                        className={'cursor-pointer'}
-                    />
-                </div>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                    <Image
-                        src={"https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-coffee-pot-and-coffee-cup-on-window-sill.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
-                        alt={""}
-                        width={40}
-                        height={40}
-                        className={'w-10 h-10 rounded-full object-cover'}
-                    />
-                    <span className={'font-semibold'}>John Wick</span>
-                </div>
-                <div className="flex gap-3 justify-end">
-                    <Image
-                        src={"/accept.png"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                        className={'cursor-pointer'}
-                    />
-                    <Image
-                        src={"/reject.png"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                        className={'cursor-pointer'}
-                    />
-                </div>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                    <Image
-                        src={"https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-coffee-pot-and-coffee-cup-on-window-sill.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
-                        alt={""}
-                        width={40}
-                        height={40}
-                        className={'w-10 h-10 rounded-full object-cover'}
-                    />
-                    <span className={'font-semibold'}>John Wick</span>
-                </div>
-                <div className="flex gap-3 justify-end">
-                    <Image
-                        src={"/accept.png"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                        className={'cursor-pointer'}
-                    />
-                    <Image
-                        src={"/reject.png"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                        className={'cursor-pointer'}
-                    />
-                </div>
-            </div>
+            <FriendRequestList requests={requests} />
         </div>
     )
 }
