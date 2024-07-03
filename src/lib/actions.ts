99e33bbec4c9f2f6f6e545700281d8fa8866b2c1
Takhir <guvalakat1603@gmail.com>
2024-07-03 16:02:19 +0500

    updateProfile action is created with using zod dependency

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import {auth} from "@clerk/nextjs/server";
 import prisma from "@/lib/client";
+import {z} from "zod";
 
 export const switchFollow = async (userId: string) => {
     const {userId: currentUserId} = auth();
@@ -163,4 +164,53 @@ export const declineFollowRequest = async (userId: string) => {
         console.log(error)
         throw new Error("Something went wrong");
     }
+}
+
+export const updateProfile = async (formData: FormData) => {
+
+    const fields = Object.fromEntries(formData);
+
+    const filteredFields = Object.fromEntries(
+        Object.entries(fields).filter(([_,value]) => value !== "")
+    );
+
+    console.log(fields);
+
+    const Profile = z.object({
+        cover: z.string().optional(),
+        name: z.string().max(60).optional(),
+        surname: z.string().max(60).optional(),
+        description: z.string().max(60).optional(),
+        city: z.string().max(60).optional(),
+        school: z.string().max(60).optional(),
+        work: z.string().max(60).optional(),
+        website: z.string().max(60).optional(),
+    });
+
+    const validatedFields = Profile.safeParse(filteredFields);
+
+    if (!validatedFields.success){
+        console.log(validatedFields.error.flatten().fieldErrors);
+        return validatedFields.error.flatten().fieldErrors;
+    }
+
+    const {userId: currentUserId} = auth();
+
+    if (!currentUserId) {
+        throw new Error("User is not authenticated!.");
+    }
+
+    try {
+        await prisma.user.update({
+            where: {
+                id: currentUserId,
+            },
+            data: validatedFields.data
+        });
+    }
+    catch (err){
+        console.log(err)
+        throw new Error("Something went wrong");
+    }
+
 }
